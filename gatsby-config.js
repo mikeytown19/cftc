c864fc586c1756805f4b99cb66d3c7f490cf974a
Michael Harvey <mikeytown19@gmail.com>
2018-10-28 21:43:45 -0600

    fixed styled components build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
-    `gatsby-plugin-styled-components`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
